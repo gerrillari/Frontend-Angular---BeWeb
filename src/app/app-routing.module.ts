8c32bc75bed4a73bbf396adb7ae48debc8199742
gerrillari <tam.alcalajimenez@gmail.com>
2021-07-08 12:37:10 +0000

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CommonModule } from '@angular/common';
+import { MessageswritingComponent } from './messaging/messageswriting/messageswriting.component';
+import { InboxComponent } from './messaging/inbox/inbox.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes = [
+  {
+    path:"", component: DashboardComponent,
+  },
+  {
+    path: "messages", component: InboxComponent,
+  },
+  {
+    path: "create-message", component: MessageswritingComponent,
+  }
+] as Routes
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),CommonModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
